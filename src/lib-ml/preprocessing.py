0e7cf264c4defe076ea5e90be4ef2e744c2bea96
Sam <s.goldie@student.maastrichtuniversity.nl>
2025-05-04 23:13:08 +0200

    refactoring

--- src/lib-ml/preprocessing.py
+++ src/lib-ml/preprocessing.py
@@ -9,24 +9,28 @@ class TextPreprocessor:
         self.all_stopwords = self._load_stopwords()
         
     def _load_stopwords(self):
+        """Load English stopwords, excluding 'not'"""
         nltk.download('stopwords', quiet=True)
         all_stopwords = stopwords.words('english')
         all_stopwords.remove('not')
         return all_stopwords
         
     def preprocess_text(self, text):
-        if not isinstance(text, str):
-            return ""
-            
-        review = re.sub('[^a-zA-Z]', ' ', text)
+        """Clean and preprocess a single review text"""
+        review = re.sub('[^a-zA-Z]', ' ', str(text))
         review = review.lower()
         review = review.split()
         review = [self.ps.stem(word) for word in review if word not in set(self.all_stopwords)]
         return ' '.join(review)
         
     def preprocess_texts(self, texts):
+        """Process multiple texts"""
         return [self.preprocess_text(text) for text in texts]
-    
-
-if __name__ == '__main__':
-    pass
+        
+    def get_corpus(self, dataset, text_column='Review'):
+        """Get corpus from dataset - for backward compatibility"""
+        corpus = []
+        for i in range(len(dataset)):
+            review = self.preprocess_text(dataset[text_column][i])
+            corpus.append(review)
+        return corpus
